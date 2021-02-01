84c7cc5f511bc820196dbc3bc6da6a573a96ed0e
ffelipedev <ffelipedev@gmail.com>
2021-02-01 10:07:57 -0500

    :star2:

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/Biblioteca.dart';
+import 'package:youtube/telas/EmAlta.dart';
+import 'package:youtube/telas/Inicio.dart';
+import 'package:youtube/telas/Inscricoes.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,8 +10,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _indiceAtual = 0;
   @override
   Widget build(BuildContext context) {
+    List<Widget> telas = [Inicio(), EmAlta(), Biblioteca(), Inscricoes()];
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(
@@ -37,7 +43,42 @@ class _HomeState extends State<Home> {
               })
         ],
       ),
-      body: Container(),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        // => faz com que as cores mudem no bottombar type: BottomNavigationBarType.shifting,
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+
+        currentIndex: _indiceAtual,
+        //ao clicar trocara a cor do background do bottomBar
+        onTap: (indice) {
+          setState(() {
+            _indiceAtual = indice;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            //   backgroundColor: Colors.red,
+            label: "Inicio",
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            //  backgroundColor: Colors.green,
+            label: "em alta",
+            icon: Icon(Icons.whatshot),
+          ),
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.blue,
+            label: "Inscrições",
+            icon: Icon(Icons.subscriptions),
+          ),
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.purple,
+            label: "Biblioteca",
+            icon: Icon(Icons.folder),
+          ),
+        ],
+      ),
     );
   }
 }
